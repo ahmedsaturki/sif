476e0c7bf980d05a8bc48bbf7faa12d103b28bdf
Ahmed Saeed Turki <ahmedsaeedturki@gmail.com>
2026-09-17 01:37:16 +0300

    feat(policy): export Phase 4 governance boundary

--- packages/sif-core/src/index.ts
+++ packages/sif-core/src/index.ts
@@ -17,6 +17,7 @@ export * from "./self-model.js";
 export * from "./integrity.js";
 export * from "./cas.js";
 export * from "./policy.js";
+export * from "./policy-governance.js";
 export * from "./outbox.js";
 export * from "./postgres.js";
 

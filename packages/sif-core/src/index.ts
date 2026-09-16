a758cd763926540ed772139c30ab7801dd410dd9
Ahmed Saeed Turki <ahmedsaeedturki@gmail.com>
2026-09-16 05:12:21 +0300

    Add complete SIF Core source: public index

---
+++ packages/sif-core/src/index.ts
@@ -0,0 +1,16 @@
+export * from "./types.js";
+export * from "./core.js";
+
+export * from "./persistence.js";
+export * from "./federation.js";
+export * from "./self-model.js";
+export * from "./integrity.js";
+export * from "./cas.js";
+export * from "./policy.js";
+export * from "./outbox.js";
+export * from "./postgres.js";
+
+export * from "./replay.js";
+export * from "./cas-metadata.js";
+export * from "./worker.js";
+export * from "./projector.js";

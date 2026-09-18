577654a035a081fe9d3e91b9e2f2ad4c18158b3e
Ahmed Saeed Turki <ahmedsaeedturki@gmail.com>
2026-09-18 03:55:22 +0300

    fix: run resolution tests against compiled output

--- integrations/lara-os-reie-runtime/test/local-core.test.ts
+++ integrations/lara-os-reie-runtime/test/local-core.test.ts
@@ -4,7 +4,7 @@ import { mkdtemp, readFile, rm } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 import { openReieWorkspace } from "../dist/persistence.js";
-import { resolveEntityCandidates } from "../resolution.js";
+import { resolveEntityCandidates } from "../dist/resolution.js";
 import { generateReieSignals } from "../signals.js";
 import type { ReieEntity } from "../reie.js";
 
